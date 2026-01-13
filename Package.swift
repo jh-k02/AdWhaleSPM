3ed72e2bda1d6638199e4ac208c59a917a0a04d5
jh-k02 <76684103+jh-k02@users.noreply.github.com>
2026-01-13 15:09:39 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "AdWhaleSPM",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "AdWhaleSPM",
+            targets: ["AdWhaleSDKResources"]
+        )
+    ],
+    dependencies: [
+        // 1. Google Mobile Ads SDK (AdMob)
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            exact: "12.14.0"
+        ),
+
+        // 2, Cauly SDK
+        .package(
+            url: "https://github.com/cauly/CaulySPM.git",
+            exact: "3.1.22"
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AdWhaleSDK",
+            path: "AdWhaleSDK.xcframework"
+        ),
+
+        .target(
+            name: "AdWhaleSDKResources",
+            dependencies: [
+                .target(name: "AdWhaleSDK"),
+
+                // AdMob
+                .product(
+                    name: "GoogleMobileAds",
+                    package: "swift-package-manager-google-mobile-ads"
+                ),
+
+                // Cauly
+                .product(
+                    name: "CaulySDK",
+                    package: "CaulySPM"
+                ),
+        )
+    ]
+)
