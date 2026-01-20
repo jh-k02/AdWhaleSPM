9250aaae365166e743b9bfadf67205b8c1f487fe
김주현 <juhyunk2@fsn.co.kr>
2026-01-20 14:18:38 +0900

    Package.swift 업데이트

--- Package.swift
+++ Package.swift
@@ -12,19 +12,6 @@ let package = Package(
             targets: ["AdWhaleSDKResources"]
         )
     ],
-    dependencies: [
-        // 1. Google Mobile Ads SDK (AdMob)
-        .package(
-            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
-            exact: "12.14.0"
-        ),
-
-        // 2, Cauly SDK
-        .package(
-            url: "https://github.com/cauly/CaulySPM.git",
-            exact: "3.1.22"
-        )
-    ],
     targets: [
         .binaryTarget(
             name: "AdWhaleSDK",
@@ -34,20 +21,7 @@ let package = Package(
         .target(
             name: "AdWhaleSDKResources",
             dependencies: [
-                // .target(name: "AdWhaleSDK"),
-                "AdWhaleSDK",
-
-                // AdMob
-                .product(
-                    name: "GoogleMobileAds",
-                    package: "swift-package-manager-google-mobile-ads"
-                ),
-
-                // Cauly
-                .product(
-                    name: "CaulySDK",
-                    package: "CaulySPM"
-                )
+                .target(name: "AdWhaleSDK")
             ]
         )
     ]
